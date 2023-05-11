aefe62f67c5e237011ae1c4a443f4ceb62d72446
adrianbetea <beteaadrian03gmail.com>
2023-05-11 19:15:02 +0300

    LIB-2 - connection between Home and Login forms added

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,13 +11,29 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.Parent;
 
+import java.io.IOException;
+
 
 public class HomeController {
-    @FXML
-    private Button button1;
+    private Stage login_stage;
+    private Scene login_scene;
+    ;
+    private Parent root;
 
-    @FXML
-    private Button button2;
+    public void loginManager(ActionEvent login_event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("login-manager.fxml"));
+        login_stage = (Stage)((Node)login_event.getSource()).getScene().getWindow();
+        login_scene = new Scene(root);
+        login_stage.setScene(login_scene);
+        login_stage.show();
+    }
+    public void loginUser(ActionEvent login_event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("login-user.fxml"));
+        login_stage = (Stage)((Node)login_event.getSource()).getScene().getWindow();
+        login_scene = new Scene(root);
+        login_stage.setScene(login_scene);
+        login_stage.show();
+    }
 
 
 }
